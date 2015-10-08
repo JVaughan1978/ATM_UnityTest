173f302115dc4041bb939aa44d8b18e64d9fb78e
Joshua Vaughan <Joshua.S.Vaughan@gmail.com>
2015-10-08 15:56:26 -0400

    Adding Title Sizing
    Adjusts the size and position of the title text based on the screens
    height and width. Going to address Screen.orientation next...

---
+++ Assets/Scripts/TitleSizing.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof (Text))]
+public class TitleSizing : MonoBehaviour {
+
+    // I want to change font size and position of this object based on screen resolution
+    private int fontSize = 0;
+    private int topOffset = 0;
+    private int rectWidth = 0;
+    private int rectHeight = 0;
+
+    private Text m_text = null;
+
+    int GetFontSize() 
+    {
+        int ret = 0;
+        if (Screen.height >= Screen.width) 
+        {
+            ret = (int)((float)Screen.height * 0.03f);
+        }
+        else 
+        {
+            ret = (int)((float)Screen.width * 0.03f); 
+        }
+        return ret;
+    }
+
+    int GetTopOffset() 
+    {
+        int ret = 0;
+        ret = (int)((float)Screen.height * 0.5f) - (int)((float)fontSize * 1.5f);
+        return ret;
+    }   
+
+	// Use this for initialization
+	void Start () 
+    {	
+        m_text = GetComponent<Text>();
+        fontSize = GetFontSize();
+        topOffset = GetTopOffset();
+        rectHeight = fontSize * 2;
+        rectWidth = fontSize * 13;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        Debug.Log(topOffset);
+        m_text.fontSize = fontSize;        
+        m_text.rectTransform.localPosition = new Vector3( 0, topOffset, 0);
+        m_text.rectTransform.sizeDelta = new Vector2(rectWidth, rectHeight);
+	}
+}
