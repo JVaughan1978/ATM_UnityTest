4ad3c4a7db0b42a0464c47be11dda00c400af877
Joshua Vaughan <Joshua.S.Vaughan@gmail.com>
2015-10-08 23:22:43 -0400

    Added resizable box

---
+++ Assets/Scripts/ColliderTransform.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColliderTransform : MonoBehaviour 
+{
+    public GameObject camera = null; // set by user;   
+
+    private int _lastScreenWidth = 0;
+    private int _lastScreenHeight = 0;
+    private bool allSet = false;
+
+    public float scalarConstant = 1.0f;
+    
+    void SetPosition() 
+    {
+        Camera cam = camera.GetComponent<Camera>();
+        Vector3 offsetPosition = new Vector3(((float)Screen.width * 0.42f),((float)Screen.height * 0.5f), 10.0f);
+        Vector3 position = cam.ScreenToWorldPoint(offsetPosition);
+        transform.position = position;
+    }
+
+    void SetScale() 
+    {
+        //need the magic number from the screens
+        float scalarX = 1.0f;
+        float scalarY = 1.0f;
+        float scalarZ = 1.0f;
+
+        if(Screen.width > Screen.height) 
+        {
+            scalarX = (float)Screen.width / (float)Screen.height;
+            scalarY = 1.0f / 0.8f;
+        }
+        else 
+        {
+            scalarY = (float)Screen.height / (float)Screen.width;
+            scalarY = 1.0f / 0.8f;
+        }
+
+        Vector3 scale = new Vector3(scalarX * scalarConstant, scalarY * scalarConstant, scalarZ * scalarConstant);
+        Debug.Log(scale);
+        transform.localScale = scale;
+    }
+
+	// Use this for initialization
+	void Start () {        
+        _lastScreenWidth = Screen.width;
+        _lastScreenHeight = Screen.height;
+        SetPosition();
+        SetScale();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(_lastScreenHeight != Screen.height || _lastScreenWidth != Screen.width) 
+        {
+            allSet = false;
+        }        
+
+        if(!allSet) 
+        {
+            SetPosition();
+            SetScale();
+            allSet = true;
+        }
+
+        _lastScreenHeight = Screen.height;
+        _lastScreenWidth = Screen.width;	
+	}
+}
