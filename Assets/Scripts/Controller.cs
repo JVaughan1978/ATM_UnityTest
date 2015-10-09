71fdbccc0a72d8b7001eda7df32164d95c3ae1e7
Joshua Vaughan <Joshua.S.Vaughan@gmail.com>
2015-10-08 21:30:51 -0400

    Images Loading, image loading error handling in place

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -10,19 +10,31 @@ public class Controller : MonoBehaviour {
 
     public string url = "";
     public RootObject data;
-    public static bool JSON_LOAD_COMPLETE = false;
-    public static bool OBJECTS_CREATED = false;
+    private bool JSON_LOAD_COMPLETE = false;
+    private bool TITLE_CREATED = false;
+    private bool IMAGES_LOADED = false;
+    private bool SPRITES_CREATED = false;
+    private bool BUTTONS_CREATED = false;
+    private bool MENU_CREATED = false;    
 
     public GameObject canvas = null;
-    public Font defaultFont = null;
+    public Font defaultFont = null; //set in editor
+
+    //private int currentLoadedImageCount = 0;
+    private int totalImages = 99;
+    private int imagesLoaded = 0;
+    public Texture2D defaultTex2D = null; //set in editor
+
+    public List<Texture2D> images = new List<Texture2D>();
+    public List<Sprite> sprites = new List<Sprite>();        
     
     public class Button 
     {
         public string title { get; set; }
-        public string imgLocation { get; set; } //going to need to www fetch these images on the material level accessing this
-        public string color { get; set; } // going to need to convert this string from 3 digits of hex to a Unity Color type
-        public string type { get; set; } // a simple switch to choose between sphere or cube as the primative
-        public bool obeyGravity { get; set; } //setting flags on the created object                
+        public string image { get; set; } 
+        public string color { get; set; } 
+        public string type { get; set; } 
+        public bool obeyGravity { get; set; }
     }
     
     public class RootObject 
@@ -32,7 +44,7 @@ public class Controller : MonoBehaviour {
         public List<Button> buttons { get; set; }
     }    
 
-    public List<GameObject> genObjects = new List<GameObject>();
+    public List<GameObject> genObjects = new List<GameObject>();    
 
     IEnumerator WWWGet() 
     {
@@ -95,6 +107,78 @@ public class Controller : MonoBehaviour {
         go.AddComponent<TitleSizing>();
     }
 
+    void GetImages() 
+    {
+        totalImages = data.buttons.Count;
+        Debug.Log(totalImages);
+
+        for (int i = 0; i < totalImages; i++) {
+            //fill in the standins
+            Texture2D tex2D = defaultTex2D;
+            images.Add(tex2D);
+            //then fetch the images from the web
+            StartCoroutine(WWWGetImages(data.buttons[i].image, i));            
+        }       
+    }
+
+    IEnumerator WWWGetImages(string url, int listPos) 
+    {        
+        WWW www = new WWW(url);
+        yield return www;
+        
+        if (!string.IsNullOrEmpty(www.error)) 
+        {
+            Debug.LogError("Image " + url + " not loaded.");
+            data.buttons[listPos].image = null;
+            --totalImages;           
+        }
+        else 
+        {
+            images[listPos] = new Texture2D(400, 300);
+            www.LoadImageIntoTexture(images[listPos]);
+            images[listPos].filterMode = FilterMode.Bilinear;
+            ++imagesLoaded;
+        }        
+    }
+
+    void CreateButtons() 
+    {
+        int iterator = 0;
+
+        foreach (Button button in data.buttons) 
+        {
+            if (button.image != null) 
+            {
+                //create a sprite
+                Sprite tempSprite = Sprite.Create(images[iterator], new Rect(Vector2.zero, Vector2.one), new Vector2(0.5f, 0.5f));
+                sprites.Add(tempSprite);
+                //build the button GameObject;
+                GameObject go = new GameObject();
+                go.name = data.buttons[iterator].title;
+                UnityEngine.UI.Image uiSprite = go.AddComponent<UnityEngine.UI.Image>();
+                UnityEngine.UI.Button uiButton = go.AddComponent<UnityEngine.UI.Button>();
+                uiButton.targetGraphic = uiSprite;
+                GameObject textGO = new GameObject();
+                textGO.name = "Text";
+                Text text = textGO.AddComponent<Text>();
+                text.text = data.buttons[iterator].title;
+                //going to need some more positional stuff for the scroll window
+                textGO.transform.SetParent(uiButton.gameObject.transform);
+                uiButton.gameObject.transform.SetParent(canvas.transform);
+            }            
+            iterator++;
+        }
+    }
+
+    
+
+    void CreateButton(Button data) 
+    {
+        GameObject go = new GameObject();
+        go.name = data.title;
+        go.transform.parent = canvas.transform;
+    }
+
 	void Start () 
     {
         string HtmlText = GetHtmlFromUri("http://google.com");
@@ -110,29 +194,53 @@ public class Controller : MonoBehaviour {
         } 
         else 
         {   //success
-            StartCoroutine("WWWGet");
+            StartCoroutine("WWWGet");            
         }
 
-        canvas = GameObject.Find("Canvas");
-        //make upper text
-        
-        //make scrolling buttons
-        //make bottom button
+        canvas = GameObject.Find("Canvas");        
 	}
 	
 	void Update () 
     {
-        if (!OBJECTS_CREATED && JSON_LOAD_COMPLETE) 
+        if (!TITLE_CREATED && JSON_LOAD_COMPLETE) 
         {
-            CreateTitleGameObject(data.title);
-            OBJECTS_CREATED = true;
+            CreateTitleGameObject(data.title);            
+            TITLE_CREATED = true;
         }
 
-        if(Input.GetKeyUp(KeyCode.Space)){
+        if (!IMAGES_LOADED && JSON_LOAD_COMPLETE) 
+        {
+            GetImages();
+            IMAGES_LOADED = true;            
+        }
+        
+        //then sprites and buttons if possible
+        if (imagesLoaded == totalImages && !BUTTONS_CREATED) {
+            CreateButtons();
+            //work on the scrolling list;
+            BUTTONS_CREATED = true;
+        }
+        
+        //then build the menu
+
+        if (Input.GetKeyUp(KeyCode.B)) 
+        {
+            GameObject go = new GameObject();
+            go.name = data.buttons[0].title;
+            UnityEngine.UI.Image uiSprite = go.AddComponent<UnityEngine.UI.Image>();
+            UnityEngine.UI.Button uiButton = go.AddComponent<UnityEngine.UI.Button>();
+            uiButton.targetGraphic = uiSprite;
+            GameObject text = new GameObject();
+            text.name = "Text";
+            text.transform.SetParent(uiButton.gameObject.transform);
+            uiButton.gameObject.transform.SetParent(canvas.transform);
+        }
+
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
             Debug.Log(data.title);
             Debug.Log(data.maxObjects);
             Debug.Log(data.buttons.Count);
-        }
-        
+        }        
     }
 }
